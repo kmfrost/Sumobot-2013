5d7c7e7e370bbca03db67c007782c56bfb4cabf8
Ben Straub <straub@auburn.edu>
2013-01-09 17:14:17 -0600

    Adding my SR IR sensor test, servo test, and Kayla's line sensor test.

---
+++ SR_sensor_test/SR_sensor_test.ino
@@ -0,0 +1,19 @@
+int sensorPin = A5;
+int sensorVal;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+void loop()
+{
+  sensorVal = digitalRead(sensorPin);
+  Serial.print(sensorVal);
+  if (sensorVal)
+  {
+    Serial.println("\t\tYES!");
+  } else {
+    Serial.println("\tNO!");
+  }
+  delay(200);
+}
---
+++ servo_test/servo_test.ino
@@ -0,0 +1,33 @@
+#include <Servo.h>
+
+int motorPin = 3;
+unsigned short pos = 0;
+Servo motor;
+
+void setup()
+{
+  Serial.begin(9600);
+  motor.attach(motorPin);
+//  pinMode(motorPin, OUTPUT);
+//  analogWrite(motorPin, 0);
+}
+void loop()
+{
+  char temp;
+  if (Serial.available())
+    temp = Serial.read();
+  if (temp >= 0x30 && temp <= 0x39)
+  {
+    pos = (pos*10)+(temp-0x30);
+    Serial.print("temp: ");
+    Serial.print(temp);
+    Serial.print("\tpos: ");
+    Serial.print(pos);
+  }
+  else if (temp == 'w')
+  {
+    motor.write((pos));
+    Serial.println(pos);
+    pos = 0;
+  }
+}
