a91917c41018b49aaaf99ee9fc20afd1749f8112
Kayla Frost <kmf0008@auburn.edu>
2013-01-09 17:09:40 -0600

    Adding short range and long range ir test code

---
+++ LR_IR_TEST.ino
@@ -0,0 +1,17 @@
+int sensorOut = A0;
+int val = 0;
+
+void setup()
+{
+  Serial.begin(9600);               // starts the serial monitor
+  pinMode(sensorOut, INPUT);
+}
+
+void loop()
+{
+  delay(500);
+  val = analogRead(sensorOut);       // reads the value of the sharp sensor
+  Serial.println(val);               // prints the value of the sensor to the serial monitor
+}
+
+
---
+++ SR_IR_TEST.ino
@@ -0,0 +1,22 @@
+int sensorOut = 51;
+int sensorVin = 53;
+boolean val = 0;
+
+void setup()
+{
+  Serial.begin(9600);               // starts the serial monitor
+  
+  pinMode(sensorOut, INPUT);
+  pinMode(sensorVin, OUTPUT);
+
+  digitalWrite(sensorVin, HIGH);
+}
+
+void loop()
+{
+  val = digitalRead(sensorOut);       // reads the value of the sharp sensor
+  Serial.println(val);               // prints the value of the sensor to the serial monitor
+  delay(500);
+}
+
+
