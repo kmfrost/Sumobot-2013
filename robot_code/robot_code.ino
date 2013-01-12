a29437d94c4dd8252ef3a3afea49df6311b3963e
Kayla Frost <kmf0008@auburn.edu>
2013-01-11 22:50:21 -0600

    Updating code with some scanning stuffs.

--- robot_code/robot_code.ino
+++ robot_code/robot_code.ino
@@ -14,7 +14,9 @@ int mcLDPin = 0;      //Left motor controller "D" pin
 int mcREnPin = 0;     //Right motor controller enable pin - Must be PWM
 int mcRCPin = 0;      //Right motor controller "C" pin
 int mcRDPin = 0;      //Right motor controller "D" pin 
-int scanvel = 0;    //Ideal vel for scanning with sensors
+int scanVel = 0;      //Ideal vel for scanning with sensors
+int LEFT = 0;         //Define left and right as integers to minimize memory usage
+int RIGHT = 1;        
 
 Servo motor;          //Instantiate the motor
 
@@ -43,7 +45,7 @@ void loop()
     Serial.print("Button pressed!");
   #endif
   motor.write(downPos); //Drop the plate
-  delay(5000)
+  delay(5000);
   
   //Somehow calibrate the reflective sensors  
   
@@ -51,9 +53,9 @@ void loop()
   delay(5000);
   driveBackward(100);
   delay(5000);
-  driveTurn(100, "LEFT");
+  driveTurn(100, LEFT);
   delay(5000);
-  driveTurn(100, "RIGHT");
+  driveTurn(100, RIGHT);
   delay(5000);
   driveStop(100);
   delay(1000000000);
@@ -81,24 +83,24 @@ void driveBackward(int vel) //vel (velocity) is of the range 0-255 and is a meas
    digitalWrite(mcRDPin, HIGH);
 }
 
-void driveTurn(int vel, String direct) //vel (velocity) is of the range 0-255 and is a measure of how fast to turn the motors
-{                              //direct  (direction) dictates which way to turn, "LEFT" or "RIGHT"
+void driveTurn(int vel, int direct) //vel (velocity) is of the range 0-255 and is a measure of how fast to turn the motors
+{                              //direct  (direction) dictates which way to turn, LEFT or RIGHT
   analogWrite(mcLEnPin, vel);  //Turn on the enable pin for both motor controller sides at the given speed 
   analogWrite(mcREnPin, vel);     
 
-  if (direct == "LEFT")
+  if (direct == LEFT)
   {
-   digitalWrite(mcLCPin, LOW);    //Stop the left wheel from turning - set pin C and pin D to the same value
-   digitalWrite(mcLDPin, LOW);
+   digitalWrite(mcLCPin, LOW);    //Drive the left wheel backwards - setting the pin C low and pin D high
+   digitalWrite(mcLDPin, HIGH);
    digitalWrite(mcRCPin, HIGH);   //Drive the right wheel forward - setting pin C high and pin D low
    digitalWrite(mcRDPin, LOW);
   }
-  else if (direct == "RIGHT")
+  else if (direct == RIGHT)
   {
    digitalWrite(mcLCPin, HIGH);   //Drive the left wheel forward - setting pin C high and pin D low
    digitalWrite(mcLDPin, LOW);
-   digitalWrite(mcRCPin, LOW);    //Stop the right wheel from turning - set pin C and pin D to the same value
-   digitalWrite(mcRDPin, LOW);
+   digitalWrite(mcRCPin, LOW);    //Drive the left wheel backwards - setting the pin C low and pin D high
+   digitalWrite(mcRDPin, HIGH);
   }
 }
 
@@ -113,4 +115,51 @@ void driveStop(int vel)  //vel (velocity) is of the range 0-255 and is a measure
   digitalWrite(mcRCPin, LOW);
   digitalWrite(mcRDPin, LOW);
 }
-   
+
+boolean searchForTarget()
+//Search for target by swiveling left and right at intervals increasing by [addTime]
+{
+ int baseTime = 250;
+ int addTime = 500;
+ driveTurn(scanVel, LEFT);
+ if (scan(baseTime))
+   return true; 
+ driveTurn(scanVel, RIGHT);
+ if scan(baseTime + addTime)
+   return true;
+ driveTurn(scanVel, LEFT);
+ if scan(baseTime + 2*addTime)
+   return true;
+ driveTurn(scanVel, RIGHT);
+ if scan(baseTime + 3*addTime)
+   return true;
+ driveStop(scanVel); 
+ return false;
+}
+
+boolean scan(int interval)
+//While searching for target, monitor the sonar and long range IR sensors for any objects
+{
+ int time = millis();
+ int targetTime = time + interval;
+ while(millis() < targetTime)
+ {
+   if(checkSonar() = 0){
+     driveStop(scanVel);
+     return true;}
+   if(checkLRIR() = 0){
+     driveStop(scanVel);
+     return true;}
+ }
+}
+
+boolean checkSonar()
+{
+  return 1;
+}
+
+boolean checkLRIR()
+{
+ return 1;
+}
+
