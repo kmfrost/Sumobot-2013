1da195aea76bd11cc695021649ab8a566a14d06b
Kayla Frost <kmf0008@auburn.edu>
2013-01-12 02:20:18 -0600

    Adding motor test, cleaning up #defines, adding mode flag, cleaning up scanning procedure.

---
+++ motor_test/motor_test/motor_test.ino
@@ -0,0 +1,138 @@
+#include <NewPing.h>
+#include <Servo.h>
+
+//This is the main code for BRUTUS the sumobot
+//Written by Kayla Frost and Ben Straub
+
+#define DEBUGPING_PIN
+#define UP_POS = 0;        //Raised position for the plate
+#define DOWN_POS = 110;    //Lowered position for the plate
+#define LEFT = 0;         //Define left and right as integers to minimize memory usage
+#define RIGHT = 1;        
+#define MAX_PING_DIST = 70; //Maximum distance we want to ping for (in cm)
+#define NO_TRIG = 0;       //Define strings to represent the different sensor triggers
+#define PING_TRIG = 1;
+#define LRIR_TRIG = 2;
+#define SRIR_TRIG = 3;
+
+#define PING_PIN = A5;     //Set the ping sensor pin
+#define LRIR_PIN = A0;     //Set the long range IR (LRIR) pin
+#define SRIR_PIN = A1;     //Set the short range IR (SRIR) pin
+#define SERVO_PIN = 9;     //Set servo pin - must be PWM
+#define BUTTON_PINMC_L_EN_PIN = A4;   //Set the start button pin - doesn't matter what kind of pin
+#define MC_L_EN_PIN = 3;     //Left (labed #1 on board) motor controller enable pin - Must be PWM
+#define MC_L_C_PIN = 2;      //Left motor controller "C" pin
+#define MC_L_D_PIN = 4;      //Left motor controller "D" pin
+#define MC_R_EN_PIN = 5;     //Right (labeled #2 on board) motor controller enable pin - Must be PWM
+#define MC_R_C_PIN = 7;      //Right motor controller "C" pin
+#define MC_R_D_PIN = 6;      //Right motor controller "D" pin
+
+int scanVel = 100;    //Ideal vel for scanning with sensors     
+
+
+
+Servo motor;          //Instantiate the servo
+NewPing sonar(PING_PIN, PING_PIN, MAX_PING_DIST); //NewPing setup of pin and maximum distance 
+
+
+void setup()
+{
+  #ifdef DEBUG               //opem serial connection if in debug mode
+    Serial.begin(9600);
+  #endif
+  motor.attach(SERVO_PINBUTTON_PIN);    //assign the motor object to the defined servo pin
+  pinMode(BUTTON_PIN, INPUT); //set the button pin as an input  
+  
+  pinMode(MC_L_EN_PIN, OUTPUT);
+  pinMode(MC_L_C_PIN, OUTPUT);
+  pinMode(MC_L_D_PIN, OUTPUT);
+  pinMode(MC_R_EN_PIN, OUTPUT);
+  pinMode(MC_R_C_PIN, OUTPUT);
+  pinMode(MC_R_D_PIN, OUTPUT);
+  pinMode(PING_PINLRIR_PIN, INPUT);
+  pinMode(LRIR_PIN, INPUT);
+  pinMode(SRIR_PIN, INPUT);
+
+  motor.write(UP_POS);  //Have the robot hold the plate up initially
+  while(!digitalRead(BUTTON_PIN)) { } //Wait for the button to be pressed
+  #ifdef DEBUG
+    Serial.print("Button pressed!");
+  #endif
+  motor.write(DOWN_POS); //Drop the plate
+  delay(5000);
+
+}
+
+void loop()
+{
+  //Somehow calibrate the reflective sensors  
+  
+  driveForward(100);
+  delay(5000);
+  driveBackward(100);
+  delay(5000);
+  driveTurn(100, LEFT);
+  delay(5000);
+  driveTurn(100, RIGHT);
+  delay(5000);
+  driveStop(100);
+  delay(1000000000);
+
+
+}
+
+void driveForward(int vel) //vel (velocity) is of the range 0-255 and is a measure of how fast to turn the motors
+{
+   analogWrite(MC_L_EN_PIN, vel);    //Turn on the enable pin for both motor controller sides at the given velocity
+   analogWrite(MC_R_EN_PIN, vel);
+   
+   digitalWrite(MC_L_C_PIN, HIGH);   //Driving forward calls for setting pin C high and pin D low
+   digitalWrite(MC_L_D_PIN, LOW);
+   digitalWrite(MC_R_C_PIN, HIGH);
+   digitalWrite(MC_R_D_PIN, LOW);
+}
+
+void driveBackward(int vel) //vel (velocity) is of the range 0-255 and is a measure of how fast to turn the motors
+{
+   analogWrite(MC_L_EN_PIN, vel);  //Turn on the enable pin for both motor controller sides at the given speed 
+   analogWrite(MC_R_EN_PIN, vel);
+   
+   digitalWrite(MC_L_C_PIN, LOW);   //Driving backward calls for setting pin C low and pin D high
+   digitalWrite(MC_L_D_PIN, HIGH);
+   digitalWrite(MC_R_C_PIN, LOW);
+   digitalWrite(MC_R_D_PIN, HIGH);
+}
+
+void driveTurn(int vel, int direct) //vel (velocity) is of the range 0-255 and is a measure of how fast to turn the motors
+{                              //direct  (direction) dictates which way to turn, LEFT or RIGHT
+  analogWrite(MC_L_EN_PIN, vel);  //Turn on the enable pin for both motor controller sides at the given speed 
+  analogWrite(MC_R_EN_PIN, vel);     
+
+  if (direct == LEFT)
+  {
+   digitalWrite(MC_L_C_PIN, LOW);    //Drive the left wheel backwards - setting the pin C low and pin D high
+   digitalWrite(MC_L_D_PIN, HIGH);
+   digitalWrite(MC_R_C_PIN, HIGH);   //Drive the right wheel forward - setting pin C high and pin D low
+   digitalWrite(MC_R_D_PIN, LOW);
+  }
+  else if (direct == RIGHT)
+  {
+   digitalWrite(MC_L_C_PIN, HIGH);   //Drive the left wheel forward - setting pin C high and pin D low
+   digitalWrite(MC_L_D_PIN, LOW);
+   digitalWrite(MC_R_C_PIN, LOW);    //Drive the left wheel backwards - setting the pin C low and pin D high
+   digitalWrite(MC_R_D_PIN, HIGH);
+  }
+}
+
+
+void driveStop(int vel)  //vel (velocity) is of the range 0-255 and is a measure of how quickly to stop
+{
+  analogWrite(MC_L_EN_PIN, vel);  //Turn on the enable pin for both motor controller sides at the given speed 
+  analogWrite(MC_R_EN_PIN, vel);
+  
+  digitalWrite(MC_L_C_PIN, LOW);   //Stopping  calls for setting the C and D pins to the same value
+  digitalWrite(MC_L_D_PIN, LOW);
+  digitalWrite(MC_R_C_PIN, LOW);
+  digitalWrite(MC_R_D_PIN, LOW);
+}
+
--- robot_code/robot_code.ino
+++ robot_code/robot_code.ino
@@ -1,30 +1,41 @@
 #include <NewPing.h>
 #include <Servo.h>
-#define DEBUG
 
 //This is the main code for BRUTUS the sumobot
 //Written by Kayla Frost and Ben Straub
 
-int pingPin = A5;     //Set the ping sensor pin
-int LRIRPin = A0;     //Set the long range IR (LRIR) pin
-int SRIRPin = A1;     //Set the short range IR (SRIR) pin
-int servoPin = 9;     //Set servo pin - must be PWM
-int buttonPin = A4;   //Set the start button pin - doesn't matter what kind of pin
-int mcLEnPin = 3;     //Left (labed #1 on board) motor controller enable pin - Must be PWM
-int mcLCPin = 2;      //Left motor controller "C" pin
-int mcLDPin = 4;      //Left motor controller "D" pin
-int mcREnPin = 5;     //Right (labeled #2 on board) motor controller enable pin - Must be PWM
-int mcRCPin = 7;      //Right motor controller "C" pin
-int mcRDPin = 6;      //Right motor controller "D" pin 
-int scanVel = 100;    //Ideal vel for scanning with sensors
-int LEFT = 0;         //Define left and right as integers to minimize memory usage
-int RIGHT = 1;        
-int upPos = 0;        //Raised position for the plate
-int downPos = 110;    //Lowered position for the plate
-int maxPingDist = 70; //Maximum distance we want to ping for (in cm)
 
-Servo motor;          //Instantiate the servo
-NewPing sonar(pingPin, pingPin, maxPingDist); //NewPing setup of pin and maximum distance 
+#define DEBUG
+#define UP_POS 0           //Raised position for the plate
+#define DOWN_POS 110       //Lowered position for the plate
+#define LEFT 0             //Define left and right as integers to minimize memory usage
+#define RIGHT 1        
+#define MAX_PING_DIST 70   //Maximum distance we want to ping for (in cm)
+#define NO_TRIG 0          //Define strings to represent the different sensor triggers
+#define PING_TRIG 1
+#define LRIR_TRIG 2
+#define SRIR_TRIG 3
+#define WIDE_SEARCH_MODE 1 //Define constants for the different modes
+#define FINE_SEARCH_MODE 2
+#define CHARGE_MODE 3
+
+#define PING_PIN A5        //Set the ping sensor pin
+#define LRIR_PIN A0        //Set the long range IR (LRIR) pin
+#define SRIR_PIN A1        //Set the short range IR (SRIR) pin
+#define SERVO_PIN 9        //Set servo pin - must be PWM
+#define BUTTON_PIN A4      //Set the start button pin - doesn't matter what kind of pin
+#define MC_L_EN_PIN 3      //Left (labed #1 on board) motor controller enable pin - Must be PWM
+#define MC_L_C_PIN 2       //Left motor controller "C" pin
+#define MC_L_D_PIN 4       //Left motor controller "D" pin
+#define MC_R_EN_PIN 5      //Right (labeled #2 on board) motor controller enable pin - Must be PWM
+#define MC_R_C_PIN 7       //Right motor controller "C" pin
+#define MC_R_D_PIN 6       //Right motor controller "D" pin
+
+int scanVel = 100;           //Ideal vel for scanning with sensors
+int mode = WIDE_SEARCH_MODE; //Define a global "mode" variable
+
+Servo motor;   //Instantiate the servo
+NewPing sonar(PING_PIN, PING_PIN, MAX_PING_DIST);  //NewPing setup of pin and maximum distance 
 
 
 void setup()
@@ -32,25 +43,24 @@ void setup()
   #ifdef DEBUG               //opem serial connection if in debug mode
     Serial.begin(9600);
   #endif
-  motor.attach(servoPin);    //assign the motor object to the defined servo pin
-  pinMode(buttonPin, INPUT); //set the button pin as an input  
-  
-  pinMode(mcLEnPin, OUTPUT);
-  pinMode(mcLCPin, OUTPUT);
-  pinMode(mcLDPin, OUTPUT);
-  pinMode(mcREnPin, OUTPUT);
-  pinMode(mcRCPin, OUTPUT);
-  pinMode(mcRDPin, OUTPUT);
-  pinMode(pingPin, INPUT);
-  pinMode(LRIRPin, INPUT);
-  pinMode(SRIRPin, INPUT);
+  motor.attach(SERVO_PIN);    //assign the motor object to the defined servo pin
+  pinMode(BUTTON_PIN, INPUT); //set the button pin as an input  
+  pinMode(MC_L_EN_PIN, OUTPUT);
+  pinMode(MC_L_C_PIN, OUTPUT);
+  pinMode(MC_L_D_PIN, OUTPUT);
+  pinMode(MC_R_EN_PIN, OUTPUT);
+  pinMode(MC_R_C_PIN, OUTPUT);
+  pinMode(MC_R_D_PIN, OUTPUT);
+  pinMode(PING_PIN, INPUT);
+  pinMode(LRIR_PIN, INPUT);
+  pinMode(SRIR_PIN, INPUT);
 
-  motor.write(upPos);  //Have the robot hold the plate up initially
-  while(!digitalRead(buttonPin)) { } //Wait for the button to be pressed
+  motor.write(UP_POS);  //Have the robot hold the plate up initially
+  while(!digitalRead(BUTTON_PIN)) { } //Wait for the button to be pressed
   #ifdef DEBUG
     Serial.print("Button pressed!");
   #endif
-  motor.write(downPos); //Drop the plate
+  motor.write(DOWN_POS); //Drop the plate
   delay(5000);
 
 }
@@ -58,75 +68,71 @@ void setup()
 void loop()
 {
   //Somehow calibrate the reflective sensors  
-  
-  driveForward(100);
-  delay(5000);
-  driveBackward(100);
-  delay(5000);
-  driveTurn(100, LEFT);
-  delay(5000);
-  driveTurn(100, RIGHT);
-  delay(5000);
-  driveStop(100);
-  delay(1000000000);
+
+if (mode == WIDE_SEARCH_MODE)
+  wideSearch();
+if (mode == FINE_SEARCH_MODE)  
+  fineSearch();
+if (mode == CHARGE_MODE)
+  charge();
 }
 
 void driveForward(int vel) //vel (velocity) is of the range 0-255 and is a measure of how fast to turn the motors
 {
-   analogWrite(mcLEnPin, vel);    //Turn on the enable pin for both motor controller sides at the given velocity
-   analogWrite(mcREnPin, vel);
+   analogWrite(MC_L_EN_PIN, vel);    //Turn on the enable pin for both motor controller sides at the given velocity
+   analogWrite(MC_R_EN_PIN, vel);
    
-   digitalWrite(mcLCPin, HIGH);   //Driving forward calls for setting pin C high and pin D low
-   digitalWrite(mcLDPin, LOW);
-   digitalWrite(mcRCPin, HIGH);
-   digitalWrite(mcRDPin, LOW);
+   digitalWrite(MC_L_C_PIN, HIGH);   //Driving forward calls for setting pin C high and pin D low
+   digitalWrite(MC_L_D_PIN, LOW);
+   digitalWrite(MC_R_C_PIN, HIGH);
+   digitalWrite(MC_R_D_PIN, LOW);
 }
 
 void driveBackward(int vel) //vel (velocity) is of the range 0-255 and is a measure of how fast to turn the motors
 {
-   analogWrite(mcLEnPin, vel);  //Turn on the enable pin for both motor controller sides at the given speed 
-   analogWrite(mcREnPin, vel);
+   analogWrite(MC_L_EN_PIN, vel);  //Turn on the enable pin for both motor controller sides at the given speed 
+   analogWrite(MC_R_EN_PIN, vel);
    
-   digitalWrite(mcLCPin, LOW);   //Driving backward calls for setting pin C low and pin D high
-   digitalWrite(mcLDPin, HIGH);
-   digitalWrite(mcRCPin, LOW);
-   digitalWrite(mcRDPin, HIGH);
+   digitalWrite(MC_L_C_PIN, LOW);   //Driving backward calls for setting pin C low and pin D high
+   digitalWrite(MC_L_D_PIN, HIGH);
+   digitalWrite(MC_R_C_PIN, LOW);
+   digitalWrite(MC_R_D_PIN, HIGH);
 }
 
 void driveTurn(int vel, int direct) //vel (velocity) is of the range 0-255 and is a measure of how fast to turn the motors
 {                              //direct  (direction) dictates which way to turn, LEFT or RIGHT
-  analogWrite(mcLEnPin, vel);  //Turn on the enable pin for both motor controller sides at the given speed 
-  analogWrite(mcREnPin, vel);     
+  analogWrite(MC_L_EN_PIN, vel);  //Turn on the enable pin for both motor controller sides at the given speed 
+  analogWrite(MC_R_EN_PIN, vel);     
 
   if (direct == LEFT)
   {
-   digitalWrite(mcLCPin, LOW);    //Drive the left wheel backwards - setting the pin C low and pin D high
-   digitalWrite(mcLDPin, HIGH);
-   digitalWrite(mcRCPin, HIGH);   //Drive the right wheel forward - setting pin C high and pin D low
-   digitalWrite(mcRDPin, LOW);
+   digitalWrite(MC_L_C_PIN, LOW);    //Drive the left wheel backwards - setting the pin C low and pin D high
+   digitalWrite(MC_L_D_PIN, HIGH);
+   digitalWrite(MC_R_C_PIN, HIGH);   //Drive the right wheel forward - setting pin C high and pin D low
+   digitalWrite(MC_R_D_PIN, LOW);
   }
   else if (direct == RIGHT)
   {
-   digitalWrite(mcLCPin, HIGH);   //Drive the left wheel forward - setting pin C high and pin D low
-   digitalWrite(mcLDPin, LOW);
-   digitalWrite(mcRCPin, LOW);    //Drive the left wheel backwards - setting the pin C low and pin D high
-   digitalWrite(mcRDPin, HIGH);
+   digitalWrite(MC_L_C_PIN, HIGH);   //Drive the left wheel forward - setting pin C high and pin D low
+   digitalWrite(MC_L_D_PIN, LOW);
+   digitalWrite(MC_R_C_PIN, LOW);    //Drive the left wheel backwards - setting the pin C low and pin D high
+   digitalWrite(MC_R_D_PIN, HIGH);
   }
 }
 
 
 void driveStop(int vel)  //vel (velocity) is of the range 0-255 and is a measure of how quickly to stop
 {
-  analogWrite(mcLEnPin, vel);  //Turn on the enable pin for both motor controller sides at the given speed 
-  analogWrite(mcREnPin, vel);
+  analogWrite(MC_L_EN_PIN, vel);  //Turn on the enable pin for both motor controller sides at the given speed 
+  analogWrite(MC_R_EN_PIN, vel);
   
-  digitalWrite(mcLCPin, LOW);   //Stopping  calls for setting the C and D pins to the same value
-  digitalWrite(mcLDPin, LOW);
-  digitalWrite(mcRCPin, LOW);
-  digitalWrite(mcRDPin, LOW);
+  digitalWrite(MC_L_C_PIN, LOW);   //Stopping  calls for setting the C and D pins to the same value
+  digitalWrite(MC_L_D_PIN, LOW);
+  digitalWrite(MC_R_C_PIN, LOW);
+  digitalWrite(MC_R_D_PIN, LOW);
 }
 
-boolean searchForTarget()
+boolean wideSearch()
 //Search for target by swiveling left and right at intervals increasing by [addTime]
 {
  int baseTime = 250;
@@ -147,6 +153,16 @@ boolean searchForTarget()
  return false;
 }
 
+boolean fineSearch()
+{
+ return true; 
+}
+
+void charge()
+{
+  
+}
+
 boolean scan(int interval)
 //While searching for target, monitor the sonar and long range IR sensors for any objects
 {
@@ -154,32 +170,41 @@ boolean scan(int interval)
  int targetTime = time + interval;
  while(millis() < targetTime)
  {
-   if(checkPing()){
+   if(checkSRIR() == SRIR_TRIG){
      driveStop(scanVel);
+     mode = CHARGE_MODE;
      return true;}
-   if(checkLRIR()){
+   if(checkLRIR() == LRIR_TRIG){
      driveStop(scanVel);
+     mode = CHARGE_MODE;
      return true;}
+   if(checkPing() == PING_TRIG){
+     driveStop(scanVel);
+     mode = FINE_SEARCH_MODE;
+     return true;}
+   return false;
  }
 }
 
-boolean checkPing()
+int checkPing()
 {
-  if (sonar.ping() > 0) //If the the sensor senses something
-    return true;
-  return false;
+  if (sonar.ping() > 0)                      //Check to see if the ping sensor sees anything
+    return PING_TRIG;
+  return NO_TRIG;
 }
 
-boolean checkLRIR()
+int checkLRIR()
 {
- int sensorValue = analogRead(LRIRPin);       // reads the value of the long range IR sensor
- if (sensorValue > 100)
-   return true;
- return false;
+ int LRIRvalue = analogRead(LRIR_PIN);       // reads the value of the long range IR sensor
+ if (LRIRvalue > 100)
+   return LRIR_TRIG;
+ return NO_TRIG;
 }
 
-boolean checkSRIR()
+int checkSRIR()
 {
-return !digitalRead(SRIRPin);
+int SRIRvalue = digitalRead(SRIR_PIN);      //reads the value of the short range IR sensor
+if (!SRIRvalue)
+  return SRIR_TRIG;
+return NO_TRIG;
 }
-
